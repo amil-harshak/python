d5a8b5dbe8bdea881fa5e7375aa9c0fe025a3bd0
amil-harshak <147579641+amil-harshak@users.noreply.github.com>
2023-10-12 10:29:02 +0530

    Create word_count.py

---
+++ word_count.py
@@ -0,0 +1,20 @@
+string = input("Enter the string : ")
+
+str = string.split(" ")
+
+print(type(str))
+
+dict={}
+
+for i in str:
+    if i in dict:
+        dict[i] = dict[i]=dict[i]+1
+    else:
+        dict[i]=1
+
+
+print(dict)        
+      
+
+count.py
+Displaying count.py.
